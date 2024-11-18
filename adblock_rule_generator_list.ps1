5bdd0637d8e8ba3506b607faf3342c312a75762d
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2024-11-18 13:29:23 +0800

    Update adblock_rule_generator_list.ps1
    Signed-off-by: GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>

--- adblock_rule_generator_list.ps1
+++ adblock_rule_generator_list.ps1
@@ -248,7 +248,7 @@ $($formattedRules -join "`n")
 "@
 
 # 定义输出文件路径
-$outputPath = "$PSScriptRoot/adblock_reject.txt"
+$outputPath = "$PSScriptRoot/adblock_reject.list"
 $textContent | Out-File -FilePath $outputPath -Encoding utf8
 
 # 输出生成的有效规则总数
